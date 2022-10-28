d2d99a272019deb5fa1e7ee0508d83f0dbe64601
Marta Enachioiu <marta.enachioiu@gmail.com>
2022-10-28 09:41:58 +0300

    Create Login/Signin REST API

---
+++ src/main/java/com/springboot/blog/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.LoginDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticate(@RequestBody LoginDto loginDto){
+       Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginDto.getUsernameOrEmail(), loginDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!", HttpStatus.OK);
+    }
+
+}
